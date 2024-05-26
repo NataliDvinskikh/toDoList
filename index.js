1f9d93fbe92f4bd0b1f48b8934a11888d1bb0ce1
Natali Dvinskikh <dvinskikhnatali@gmail.com>
2024-05-26 22:30:36 +0300

    final project

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const inputField = document.querySelector('.inputField');
+const btn = document.querySelector('.btn');
+const toDoContainer = document.querySelector('.toDoContainer');
+
+function createTask() {
+  const taskItem = document.createElement('li');
+  taskItem.classList.add('toDoAdded');
+  const taskInputValue = inputField.value;
+  taskItem.textContent = taskInputValue;
+  toDoContainer.append(taskItem);
+  inputField.value = " ";
+}
+
+function checkTask(e) {
+    e.target.classList.toggle('toDoCompleted');
+}
+
+
+btn.addEventListener('click', createTask);
+toDoContainer.addEventListener('click', checkTask);
+
+
